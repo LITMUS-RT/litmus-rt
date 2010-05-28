2a94c7bf9869a13e32de7a1fe94596de7b4789a8
Andrea Bastoni <bastoni@cs.unc.edu>
2010-05-28 10:03:24 -0400

    [ported from 2008.3] Add LITRMUS^RT syscalls to x86_32

--- arch/x86/include/asm/unistd_32.h
+++ arch/x86/include/asm/unistd_32.h
@@ -343,9 +343,13 @@
 #define __NR_rt_tgsigqueueinfo	335
 #define __NR_perf_event_open	336
 
+#define __NR_LITMUS		337
+
+#include "litmus/unistd.h"
+
 #ifdef __KERNEL__
 
-#define NR_syscalls 337
+#define NR_syscalls 336 + NR_litmus_syscalls
 
 #define __ARCH_WANT_IPC_PARSE_VERSION
 #define __ARCH_WANT_OLD_READDIR
