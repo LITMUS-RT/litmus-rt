b0006637c81c31637b7be6cf80663cf317397052
Bjoern Brandenburg <bbb@mpi-sws.org>
2012-08-13 17:27:09 +0200

    Step 5: Add a scheduling function to the demo plugin
    This doesn't actually work yet because we don't handle tasks that
    resume.
    
    This patch corresponds to:
    
    	https://wiki.litmus-rt.org/litmus/CreateAPluginTutorial/Step5

--- litmus/sched_demo.c
+++ litmus/sched_demo.c
@@ -7,6 +7,8 @@
 #include <litmus/litmus.h>
 #include <litmus/rt_domain.h>
 #include <litmus/edf_common.h>
+#include <litmus/jobs.h>
+#include <litmus/budget.h>
 
 struct demo_cpu_state {
 	rt_domain_t	local_queues;
@@ -20,16 +22,96 @@ static DEFINE_PER_CPU(struct demo_cpu_state, demo_cpu_state);
 #define cpu_state_for(cpu_id)	(&per_cpu(demo_cpu_state, cpu_id))
 #define local_cpu_state()	(&__get_cpu_var(demo_cpu_state))
 
+
+/* this helper is called when task `prev` exhausted its budget of when
+ * it signaled a job completion */
+static void demo_job_completion(struct task_struct *prev, int buget_exhausted)
+{
+	/* call common helper code to compute the next release time, deadline,
+	 * etc. */
+	prepare_for_next_period(prev);
+}
+
+/* Add the task `tsk` to the appropriate queue. Assumes caller holds the ready lock.
+ */
+static void demo_requeue(struct task_struct *tsk, struct demo_cpu_state *cpu_state)
+{
+	if (is_released(tsk, litmus_clock())) {
+		/* Uses __add_ready() instead of add_ready() because we already
+		 * hold the ready lock. */
+		__add_ready(&cpu_state->local_queues, tsk);
+	} else {
+		/* Uses add_release() because we DON'T have the release lock. */
+		add_release(&cpu_state->local_queues, tsk);
+	}
+}
+
 static struct task_struct* demo_schedule(struct task_struct * prev)
 {
+	struct demo_cpu_state *local_state = local_cpu_state();
+
+	/* next == NULL means "schedule background work". */
+	struct task_struct *next = NULL;
+
+	/* prev's task state */
+	int exists, out_of_time, job_completed, self_suspends, preempt, resched;
+
+	raw_spin_lock(&local_state->local_queues.ready_lock);
+
+	BUG_ON(local_state->scheduled && local_state->scheduled != prev);
+	BUG_ON(local_state->scheduled && !is_realtime(prev));
+
+	exists = local_state->scheduled != NULL;
+	self_suspends = exists && !is_running(prev);
+	out_of_time   = exists && budget_enforced(prev)
+		&& budget_exhausted(prev);
+	job_completed = exists && is_completed(prev);
+
+	/* preempt is true if task `prev` has lower priority than something on
+	 * the ready queue. */
+	preempt = edf_preemption_needed(&local_state->local_queues, prev);
+
+	/* check all conditions that make us reschedule */
+	resched = preempt;
+
+	/* if `prev` suspends, it CANNOT be scheduled anymore => reschedule */
+	if (self_suspends)
+		resched = 1;
+
+	/* also check for (in-)voluntary job completions */
+	if (out_of_time || job_completed) {
+		demo_job_completion(prev, out_of_time);
+		resched = 1;
+	}
+
+	if (resched) {
+		/* First check if the previous task goes back onto the ready
+		 * queue, which it does if it did not self_suspend.
+		 */
+		if (exists && !self_suspends)
+			demo_requeue(prev, local_state);
+		next = __take_ready(&local_state->local_queues);
+	} else
+		/* No preemption is required. */
+		next = local_state->scheduled;
+
+	local_state->scheduled = next;
+
+
+	if (exists && prev != next)
+		TRACE_TASK(prev, "descheduled.\n");
+	if (next)
+		TRACE_TASK(next, "scheduled.\n");
+
 	/* This mandatory. It triggers a transition in the LITMUS^RT remote
 	 * preemption state machine. Call this AFTER the plugin has made a local
 	 * scheduling decision.
 	 */
 	sched_state_task_picked();
 
-	/* We don't schedule anything for now. NULL means "schedule background work". */
-	return NULL;
+	raw_spin_unlock(&local_state->local_queues.ready_lock);
+
+	return next;
 }
 
 static long demo_admit_task(struct task_struct *tsk)
