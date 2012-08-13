7eb91919c0b574d6b249d813f2e522dc95edd26e
Bjoern Brandenburg <bbb@mpi-sws.org>
2012-08-13 15:40:39 +0200

    Step 2: Implement a basic dummy plugin
    This doesn't do anything yet.
    
    This patch corresponds to:
    
    	https://wiki.litmus-rt.org/litmus/CreateAPluginTutorial/Step2

---
+++ litmus/sched_demo.c
@@ -0,0 +1,36 @@
+
+
+#include <litmus/sched_plugin.h>
+#include <litmus/preempt.h>
+
+static struct task_struct* demo_schedule(struct task_struct * prev)
+{
+	/* This mandatory. It triggers a transition in the LITMUS^RT remote
+	 * preemption state machine. Call this AFTER the plugin has made a local
+	 * scheduling decision.
+	 */
+	sched_state_task_picked();
+
+	/* We don't schedule anything for now. NULL means "schedule background work". */
+	return NULL;
+}
+
+static long demo_admit_task(struct task_struct *tsk)
+{
+	/* Reject every task. */
+	return -EINVAL;
+}
+
+static struct sched_plugin demo_plugin = {
+	.plugin_name		= "DEMO",
+	.schedule		= demo_schedule,
+	.admit_task		= demo_admit_task,
+};
+
+static int __init init_demo(void)
+{
+	return register_sched_plugin(&demo_plugin);
+}
+
+module_init(init_demo);
+
