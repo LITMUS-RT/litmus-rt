9f2cc91ad895214eafafe61eded30ad1c0b145bf
Jonathan Herman <hermanjl@cs.unc.edu>
2012-09-27 20:10:18 -0400

    Cleanup. Remove this commit

--- litmus/litmus.c
+++ litmus/litmus.c
@@ -385,7 +385,6 @@ void litmus_exit_task(struct task_struct* tsk)
 		sched_trace_task_completion(tsk, 1);
 		sched_trace_task_exit(tsk);
 		sched_trace_task_tardy(tsk);
-		printk(KERN_WARNING "Swear I'm doing \n");
 
 		litmus->task_exit(tsk);
 
