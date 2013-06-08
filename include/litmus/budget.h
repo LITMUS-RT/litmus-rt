LITMUS-RT/litmus-rt

commit 4ffefb822b9d65d4efbedb60e3c9a0e76895cc5b

Changed completion flag use to remove race condition in Cedf,Gedf.

include/litmus/budget.h
@@ -29,7 +29,7 @@ static inline int requeue_preempted_job(struct task_struct* t)
 	/* Add task to ready queue only if not subject to budget enforcement or
 	 * if the job has budget remaining. t may be NULL.
 	 */
-	return t && (!budget_exhausted(t) || !budget_enforced(t));
+	return t && !is_completed(t) && (!budget_exhausted(t) || !budget_enforced(t));
 }
 
 #endif

litmus/jobs.c
@@ -28,7 +28,7 @@ void prepare_for_next_period(struct task_struct *t)
 	 * release and deadline. Lateness may be negative.
 	 */
 	t->rt_param.job_params.lateness =
-		(long long)litmus_clock() - 
+		(long long)litmus_clock() -
 		(long long)t->rt_param.job_params.deadline;
 
 	setup_release(t, get_release(t) + get_rt_period(t));

litmus/sync.c
@@ -50,12 +50,20 @@ static long do_wait_for_ts_release(void)
 	ret = wait_for_completion_interruptible(&wait.completion);
 
 	if (!ret) {
+		/* Setting this flag before releasing ensures that this CPU
+		 * will be the next CPU to requeue the task on a ready or
+		 * release queue.
+		 */
+		tsk_rt(current)->completed = 1;
+		mb();
+
 		/* Completion succeeded, setup release. */
 		litmus->release_at(current, wait.ts_release_time
 				   + current->rt_param.task_params.phase
 				   - current->rt_param.task_params.period);
-		/* trigger advance to next job release at the programmed time */
-		ret = complete_job();
+
+		schedule();
+		ret = 0;
 	} else {
 		/* We were interrupted, must cleanup list. */
 		mutex_lock(&task_release_lock);

litmus/sched_cedf.c
@@ -171,7 +171,6 @@ static noinline void link_task_to_cpu(struct task_struct* linked,
 
 	/* Link new task to CPU. */
 	if (linked) {
-		tsk_rt(linked)->completed = 0;
 		/* handle task is already scheduled somewhere! */
 		on_cpu = linked->rt_param.scheduled_on;
 		if (on_cpu != NO_CPU) {
@@ -350,7 +349,7 @@ static noinline void job_completion(struct task_struct *t, int forced)
 	TRACE_TASK(t, "job_completion().\n");
 
 	/* set flags */
-	tsk_rt(t)->completed = 1;
+	tsk_rt(t)->completed = 0;
 	/* prepare for next period */
 	prepare_for_next_period(t);
 	if (is_early_releasing(t) || is_released(t, litmus_clock()))

litmus/sched_gsn_edf.c
@@ -173,7 +173,6 @@ static noinline void link_task_to_cpu(struct task_struct* linked,
 
 	/* Link new task to CPU. */
 	if (linked) {
-		tsk_rt(linked)->completed = 0;
 		/* handle task is already scheduled somewhere! */
 		on_cpu = linked->rt_param.scheduled_on;
 		if (on_cpu != NO_CPU) {
@@ -341,7 +340,7 @@ static noinline void job_completion(struct task_struct *t, int forced)
 	TRACE_TASK(t, "job_completion().\n");
 
 	/* set flags */
-	tsk_rt(t)->completed = 1;
+	tsk_rt(t)->completed = 0;
 	/* prepare for next period */
 	prepare_for_next_period(t);
 	if (is_early_releasing(t) || is_released(t, litmus_clock()))

commit 39d902832855cbd2baf45ac8943879be3ad8ca5a

Fix synchronous task system release support
...and also finally fix the requeue() race (hopefully).

Commit 4ffefb822b9d65d4efbedb60e3c9a0e76895cc5b broke the synchronous
task system release code. The problem is that release_at() clears the
tsk_rt(t)->completed flag. The patch thus only made the race window
smaller, but didn't close it entirely. Further, it broke the
synchronous task set release, which relies on schedule() being called
with completed == 1.

This patch fixes both problems by introducing a new flag dont_requeue,
which indicates unambiguously that a task should not be requeued by
other processors. This flag is cleared only when the job completion is
being processed, which always happens while holding the appropriate
scheduler locks, which closes the race window.

include/litmus/budget.h
@@ -29,7 +29,8 @@ static inline int requeue_preempted_job(struct task_struct* t)
 	/* Add task to ready queue only if not subject to budget enforcement or
 	 * if the job has budget remaining. t may be NULL.
 	 */
-	return t && !is_completed(t) && (!budget_exhausted(t) || !budget_enforced(t));
+	return t && !is_completed(t) && !tsk_rt(t)->dont_requeue
+		&& (!budget_exhausted(t) || !budget_enforced(t));
 }
 
 #endif

include/litmus/rt_param.h
@@ -182,6 +182,10 @@ struct rt_param {
 	/* has the task completed? */
 	unsigned int		completed:1;
 
+	/* prevent this task from being requeued on another processor (used to
+	 * coordinate GSN-EDF, C-EDF, and sync.c) */
+	unsigned int		dont_requeue:1;
+
 #ifdef CONFIG_LITMUS_LOCKING
 	/* Is the task being priority-boosted by a locking protocol? */
 	unsigned int		priority_boosted:1;

litmus/jobs.c
@@ -32,6 +32,7 @@ void prepare_for_next_period(struct task_struct *t)
 		(long long)t->rt_param.job_params.deadline;
 
 	setup_release(t, get_release(t) + get_rt_period(t));
+	tsk_rt(t)->dont_requeue = 0;
 }
 
 void release_at(struct task_struct *t, lt_t start)

litmus/sync.c
@@ -52,18 +52,22 @@ static long do_wait_for_ts_release(void)
 	if (!ret) {
 		/* Setting this flag before releasing ensures that this CPU
 		 * will be the next CPU to requeue the task on a ready or
-		 * release queue.
+		 * release queue. Cleared by prepare_for_next_period()
 		 */
-		tsk_rt(current)->completed = 1;
-		mb();
+		tsk_rt(current)->dont_requeue = 1;
 
-		/* Completion succeeded, setup release. */
+		/* Completion succeeded, setup release time. complete_job()
+		 * will indirectly cause the period to be added to the next
+		 * release time, so subtract it here. */
 		litmus->release_at(current, wait.ts_release_time
 				   + current->rt_param.task_params.phase
 				   - current->rt_param.task_params.period);
 
-		schedule();
-		ret = 0;
+		/* Advance to next job --- when complete_job() returns, the
+		 * first job has been released. Since we patched up the release
+		 * time, this occurs when all tasks synchronously release their
+		 * first job.*/
+		ret = complete_job();
 	} else {
 		/* We were interrupted, must cleanup list. */
 		mutex_lock(&task_release_lock);