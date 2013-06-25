2b46df874e7d7feb16376b5d08650deb791fc8c1
Bjoern Brandenburg <bbb@mpi-sws.org>
2013-06-25 07:30:56 +0200

    Integrate preemption state machine with Linux scheduler
    Track when a processor is going to schedule "soon".

--- arch/x86/kernel/smp.c
+++ arch/x86/kernel/smp.c
@@ -24,6 +24,7 @@
 #include <linux/cpu.h>
 #include <linux/gfp.h>
 
+#include <litmus/preempt.h>
 #include <litmus/debug_trace.h>
 
 #include <asm/mtrr.h>
@@ -269,6 +270,11 @@ void smp_reschedule_interrupt(struct pt_regs *regs)
 	/*
 	 * KVM uses this interrupt to force a cpu out of guest mode
 	 */
+
+	/* LITMUS^RT: this IPI might need to trigger the sched state machine.
+	 * Starting from 3.0 schedule_ipi() actually does something.  This may
+	 * increase IPI latencies compared with previous versions. */
+	sched_state_ipi();
 }
 
 void smp_call_function_interrupt(struct pt_regs *regs)
