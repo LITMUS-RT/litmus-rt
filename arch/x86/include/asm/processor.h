d1ef68d09e38891a70253c79e9b1968cbce5372b
Bjoern Brandenburg <bbb@mpi-sws.org>
2013-06-27 09:54:55 +0200

    Compile fix: export num_cache_leaves in asm/processor.h
    This is required to make litmus/affinity.c compile.

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -174,6 +174,11 @@ extern void init_scattered_cpuid_features(struct cpuinfo_x86 *c);
 extern unsigned int init_intel_cacheinfo(struct cpuinfo_x86 *c);
 extern void init_amd_cacheinfo(struct cpuinfo_x86 *c);
 #ifdef CONFIG_SYSFS
+/* Note: commit 04a1541828ea223169eb44a336bfad8ec0dfb46a hid num_cache_leaves,
+ * but LITMUS^RT currently still depends on it.
+ * FIXME: port LITMUS^RT's affinity-aware scheduling to use proper interfaces.
+ */
+extern unsigned short num_cache_leaves;
 extern int get_shared_cpu_map(cpumask_var_t mask,
 			       unsigned int cpu, int index);
 #endif
