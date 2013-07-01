2d433489167e0a7a8ff69dd8955631ca3ec92d00
Bjoern Brandenburg <bbb@mpi-sws.org>
2013-07-01 23:45:08 +0200

    Disable cut-to-CFS optimization in Linux scheduler
    Global plugins require that the plugin be called even if there
    currently is no real-time task executing on the local core.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2696,21 +2696,29 @@ pick_next_task(struct rq *rq, struct task_struct *prev)
 	/*
 	 * Optimization: we know that if all tasks are in
 	 * the fair class we can call that function directly:
-	 */
+
+	 * NOT IN LITMUS^RT!
+
+	 * This breaks many assumptions in the plugins.
+	 * Do not uncomment without thinking long and hard
+	 * about how this affects global plugins such as GSN-EDF.
+
 	if (likely(prev->sched_class == class &&
 		   rq->nr_running == rq->cfs.h_nr_running)) {
 		p = fair_sched_class.pick_next_task(rq, prev);
 		if (unlikely(p == RETRY_TASK))
 			goto again;
 
-		/* assumes fair_sched_class->next == idle_sched_class */
+		// assumes fair_sched_class->next == idle_sched_class
 		if (unlikely(!p))
 			p = idle_sched_class.pick_next_task(rq, prev);
 
 		return p;
-	}
+
+	*/
 
 again:
+
 	for_each_class(class) {
 		p = class->pick_next_task(rq, prev);
 		if (p) {
