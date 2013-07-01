1354972c878bf47165257269f19832aeb91d2e8e
Bjoern Brandenburg <bbb@mpi-sws.org>
2013-07-01 23:55:58 +0200

    Don't trigger load balancer in scheduler tick for LITMUS^RT

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2802,7 +2802,8 @@ void scheduler_tick(void)
 
 #ifdef CONFIG_SMP
 	rq->idle_balance = idle_cpu(cpu);
-	trigger_load_balance(rq, cpu);
+	if (!is_realtime(current))
+		trigger_load_balance(rq, cpu);
 #endif
 	rq_last_tick_reset(rq);
 
