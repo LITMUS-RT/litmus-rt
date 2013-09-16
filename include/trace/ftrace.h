95f5c2293077eb70d47ec182cdc9519bf56befd2
Glenn Elliott <gelliott@cs.unc.edu>
2013-09-16 17:26:56 -0400

    Record LITMUS^RT timestamp in ftrace records
    Patch updates ftrace.h to record a litmus_clock() time stamp
    in ftrace records.

--- include/trace/ftrace.h
+++ include/trace/ftrace.h
@@ -18,6 +18,9 @@
 
 #include <linux/ftrace_event.h>
 
+/* for litmus_clock() */
+#include <litmus/litmus.h>
+
 /*
  * DECLARE_EVENT_CLASS can be used to add a generic function
  * handlers for events. That is, if all events have the same
@@ -54,7 +57,7 @@
 #define __string(item, src) __dynamic_array(char, item, -1)
 
 #undef TP_STRUCT__entry
-#define TP_STRUCT__entry(args...) args
+#define TP_STRUCT__entry(args...) args __field( unsigned long long, __rt_ts )
 
 #undef DECLARE_EVENT_CLASS
 #define DECLARE_EVENT_CLASS(name, proto, args, tstruct, assign, print)	\
@@ -499,7 +502,7 @@ static inline notrace int ftrace_get_offsets_##call(			\
 	strcpy(__get_str(dst), (src) ? (const char *)(src) : "(null)");
 
 #undef TP_fast_assign
-#define TP_fast_assign(args...) args
+#define TP_fast_assign(args...) args; __entry->__rt_ts = litmus_clock();
 
 #undef TP_perf_assign
 #define TP_perf_assign(args...)
