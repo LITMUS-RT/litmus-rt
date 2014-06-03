8cf950fac9a19bd24ccadddbcc6c23ed27516500
Bjoern Brandenburg <bbb@mpi-sws.org>
2014-06-03 19:45:12 +0200

    GSN-EDF: remove scheduler_tick() callback

--- litmus/sched_gsn_edf.c
+++ litmus/sched_gsn_edf.c
@@ -377,32 +377,6 @@ static noinline void job_completion(struct task_struct *t, int forced)
 		gsnedf_job_arrival(t);
 }
 
-/* gsnedf_tick - this function is called for every local timer
- *                         interrupt.
- *
- *                   checks whether the current task has expired and checks
- *                   whether we need to preempt it if it has not expired
- */
-static void gsnedf_tick(struct task_struct* t)
-{
-	if (is_realtime(t) && budget_enforced(t) && budget_exhausted(t)) {
-		if (!is_np(t)) {
-			/* np tasks will be preempted when they become
-			 * preemptable again
-			 */
-			litmus_reschedule_local();
-			TRACE("gsnedf_scheduler_tick: "
-			      "%d is preemptable "
-			      " => FORCE_RESCHED\n", t->pid);
-		} else if (is_user_np(t)) {
-			TRACE("gsnedf_scheduler_tick: "
-			      "%d is non-preemptable, "
-			      "preemption delayed.\n", t->pid);
-			request_exit_np(t);
-		}
-	}
-}
-
 /* Getting schedule() right is a bit tricky. schedule() may not make any
  * assumptions on the state of the current task since it may be called for a
  * number of reasons. The reasons include a scheduler_tick() determined that it
@@ -1057,7 +1031,6 @@ static long gsnedf_deactivate_plugin(void)
 static struct sched_plugin gsn_edf_plugin __cacheline_aligned_in_smp = {
 	.plugin_name		= "GSN-EDF",
 	.finish_switch		= gsnedf_finish_switch,
-	.tick			= gsnedf_tick,
 	.task_new		= gsnedf_task_new,
 	.complete_job		= complete_job,
 	.task_exit		= gsnedf_task_exit,
