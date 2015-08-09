141fbbfcabf3b7686e6d3a6578b47b94805adb8e
Bjoern Brandenburg <bbb@mpi-sws.org>
2015-08-09 13:18:54 +0200

    Hook into kernel/exit.c to force exiting RT tasks into best-effort mode

--- kernel/exit.c
+++ kernel/exit.c
@@ -60,6 +60,8 @@
 #include <asm/pgtable.h>
 #include <asm/mmu_context.h>
 
+#include <litmus/litmus.h>
+
 extern void exit_od_table(struct task_struct *t);
 
 static void __unhash_process(struct task_struct *p, bool group_dead)
@@ -742,6 +744,15 @@ void __noreturn do_exit(long code)
 		panic("Aiee, killing interrupt handler!");
 	if (unlikely(!tsk->pid))
 		panic("Attempted to kill the idle task!");
+
+	if (unlikely(is_realtime(tsk))) {
+		/* We would like the task to be polite
+		 * and transition out of RT mode first.
+		 * Let's give it a little help.
+		 */
+		litmus_do_exit(tsk);
+		BUG_ON(is_realtime(tsk));
+	}
 
 	/*
 	 * If do_exit is called because this processes oopsed, it's possible
