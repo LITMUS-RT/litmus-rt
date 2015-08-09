a595668af66abe391bcbae9bb2dc4bd07514aa69
Bjoern Brandenburg <bbb@mpi-sws.org>
2015-08-09 13:18:54 +0200

    Hook into SCHED_DEADLINE to protect LITMUS^RT tasks
    SCHED_DEADLINE should not preempt LITMUS^RT tasks, as the LITMUS^RT
    scheduling class is positioned above the SCHED_DEADLINE policy.

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -16,6 +16,8 @@
  */
 #include "sched.h"
 
+#include <litmus/litmus.h>
+
 #include <linux/slab.h>
 
 struct dl_bandwidth def_dl_bandwidth;
@@ -650,15 +652,17 @@ static enum hrtimer_restart dl_task_timer(struct hrtimer *timer)
 	enqueue_task_dl(rq, p, ENQUEUE_REPLENISH);
 	if (dl_task(rq->curr))
 		check_preempt_curr_dl(rq, p, 0);
-	else
+	else if (!is_realtime(rq->curr))
 		resched_curr(rq);
 
 #ifdef CONFIG_SMP
 	/*
 	 * Queueing this task back might have overloaded rq, check if we need
 	 * to kick someone away.
+	 *
+	 * LITMUS^RT: don't incur this overhead if we are running a L^RT task.
 	 */
-	if (has_pushable_dl_tasks(rq)) {
+	if (has_pushable_dl_tasks(rq) && !is_realtime(rq->curr)) {
 		/*
 		 * Nothing relies on rq->lock after this, so its safe to drop
 		 * rq->lock.
@@ -1699,8 +1703,11 @@ static void switched_from_dl(struct rq *rq, struct task_struct *p)
 	 * Since this might be the only -deadline task on the rq,
 	 * this is the right place to try to pull some other one
 	 * from an overloaded cpu, if any.
+	 *
+	 * LITMUS^RT: also don't pull tasks when we are running L^RT tasks.
 	 */
-	if (!task_on_rq_queued(p) || rq->dl.dl_nr_running)
+	if (!task_on_rq_queued(p) || rq->dl.dl_nr_running
+	    || is_realtime(rq->curr))
 		return;
 
 	queue_pull_task(rq);
@@ -1714,7 +1721,7 @@ static void switched_to_dl(struct rq *rq, struct task_struct *p)
 {
 
 	/* If p is not queued we will update its parameters at next wakeup. */
-	if (!task_on_rq_queued(p))
+	if (!task_on_rq_queued(p) || is_realtime(rq->curr))
 		return;
 
 	/*
