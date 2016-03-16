6a09accd64054fd3bfd2c98242c7357d58aa4f5c
Bjoern Brandenburg <bbb@mpi-sws.org>
2016-03-16 11:37:40 +0100

    Feather-Trace: cosmetic change in latency callback
    Have symbol for traced delta. This is a purely cosmetic change that
    makes it easier to stick in a WARN_ON() during debugging...

--- litmus/trace.c
+++ litmus/trace.c
@@ -263,11 +263,12 @@ feather_callback void save_cpu_task_latency(unsigned long event,
 {
 	lt_t now = litmus_clock();
 	lt_t *when = (lt_t*) when_ptr;
+	lt_t delta = now - *when;
 
 	write_cpu_timestamp(event, TSK_RT,
 			    0,
 			    0, LOCAL_IRQ_COUNT, 0,
-			    now - *when, DO_NOT_RECORD_TIMESTAMP);
+			    delta, DO_NOT_RECORD_TIMESTAMP);
 }
 
 /* Record to remote trace buffer */
