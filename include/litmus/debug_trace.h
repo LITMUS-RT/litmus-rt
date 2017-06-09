d7f7021d1ad6c0bf8b2434a021ab69a42e4ce77e
Bjoern Brandenburg <bbb@mpi-sws.org>
2017-06-09 13:34:05 +0200

    LITMUS^RT debug trace: optionally do not pollute namespace

--- include/litmus/debug_trace.h
+++ include/litmus/debug_trace.h
@@ -13,33 +13,45 @@ void dump_trace_buffer(int max);
 extern atomic_t __log_seq_no;
 
 #ifdef CONFIG_SCHED_DEBUG_TRACE_CALLER
-#define TRACE_PREFIX "%d P%d [%s@%s:%d]: "
-#define TRACE_ARGS  atomic_add_return(1, &__log_seq_no),	\
+#define LITMUS_TRACE_PREFIX "%d P%d [%s@%s:%d]: "
+#define LITMUS_TRACE_ARGS  atomic_add_return(1, &__log_seq_no),	\
 		raw_smp_processor_id(),				\
 		__FUNCTION__, __FILE__, __LINE__
 #else
-#define TRACE_PREFIX "%d P%d: "
-#define TRACE_ARGS  atomic_add_return(1, &__log_seq_no), \
+#define LITMUS_TRACE_PREFIX "%d P%d: "
+#define LITMUS_TRACE_ARGS  atomic_add_return(1, &__log_seq_no), \
 		raw_smp_processor_id()
 #endif
 
-#define TRACE(fmt, args...)						\
-	sched_trace_log_message(TRACE_PREFIX fmt,			\
-				TRACE_ARGS,  ## args)
+#define LITMUS_TRACE(fmt, args...)						\
+	sched_trace_log_message(LITMUS_TRACE_PREFIX fmt,			\
+				LITMUS_TRACE_ARGS,  ## args)
 
-#define TRACE_TASK(t, fmt, args...)			\
-	TRACE("(%s/%d:%d) " fmt,			 \
+#define LITMUS_TRACE_TASK(t, fmt, args...)			\
+	LITMUS_TRACE("(%s/%d:%d) " fmt,			 \
 	      t ? (t)->comm : "null",			 \
 	      t ? (t)->pid : 0,				 \
 	      t ? (t)->rt_param.job_params.job_no : 0,	 \
 	      ##args)
 
-#define TRACE_CUR(fmt, args...) \
-	TRACE_TASK(current, fmt, ## args)
+#define LITMUS_TRACE_CUR(fmt, args...) \
+	LITMUS_TRACE_TASK(current, fmt, ## args)
 
-#define TRACE_WARN_ON(cond) \
+#define LITMUS_TRACE_WARN_ON(cond) \
 	if (unlikely(cond)) \
-		TRACE("WARNING: '%s' [%s@%s:%d]\n", \
+		LITMUS_TRACE("WARNING: '%s' [%s@%s:%d]\n", \
 			#cond, __FUNCTION__, __FILE__, __LINE__)
 
+#endif
+
+#ifndef LITMUS_DEBUG_TRACE_DONT_POLLUTE_NAMESPACE
+#ifndef LITMUS_DEBUG_TRACE_H_UNQUALIFIED_NAMES
+
+#define LITMUS_DEBUG_TRACE_H_UNQUALIFIED_NAMES
+#define TRACE(fmt, args...) LITMUS_TRACE(fmt, ## args)
+#define TRACE_TASK(t, fmt, args...) LITMUS_TRACE_TASK(t, fmt, ## args)
+#define TRACE_CUR(fmt, args...) LITMUS_TRACE_CUR(fmt, ## args)
+#define TRACE_WARN_ON(cond) LITMUS_TRACE_WARN_ON(cond)
+
+#endif
 #endif
